803c13ca6fabb0195c7f3e2830a5bb85c4840683
diego-acc <diego.sm.acc@gmail.com>
2021-09-21 19:51:28 +0200

    P1.0

---
+++ P1/P1/AcercaDe.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace P1
+{
+    partial class AcercaDe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AcercaDe));
+            this.bt_Aceptar = new System.Windows.Forms.Button();
+            this.textoAcercaDe = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bt_Aceptar
+            // 
+            this.bt_Aceptar.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.bt_Aceptar.Location = new System.Drawing.Point(149, 164);
+            this.bt_Aceptar.Name = "bt_Aceptar";
+            this.bt_Aceptar.Size = new System.Drawing.Size(75, 23);
+            this.bt_Aceptar.TabIndex = 0;
+            this.bt_Aceptar.Text = "Aceptar";
+            this.bt_Aceptar.UseVisualStyleBackColor = true;
+            // 
+            // textoAcercaDe
+            // 
+            this.textoAcercaDe.AutoSize = true;
+            this.textoAcercaDe.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textoAcercaDe.Location = new System.Drawing.Point(130, 126);
+            this.textoAcercaDe.Name = "textoAcercaDe";
+            this.textoAcercaDe.Size = new System.Drawing.Size(113, 26);
+            this.textoAcercaDe.TabIndex = 1;
+            this.textoAcercaDe.Text = "Práctica 1\r\nDiego Sanz Martín";
+            this.textoAcercaDe.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(126, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 120);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // AcercaDe
+            // 
+            this.AcceptButton = this.bt_Aceptar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 199);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textoAcercaDe);
+            this.Controls.Add(this.bt_Aceptar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "AcercaDe";
+            this.ShowIcon = false;
+            this.Text = "Acerca De";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_Aceptar;
+        private System.Windows.Forms.Label textoAcercaDe;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ P1/P1/RelojMain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P1
+{
+    public partial class RelojDigital : Form
+    {
+        public RelojDigital()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void salir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AcercaDe(object sender, EventArgs e)
+        {
+            AcercaDe dlg = new AcercaDe();
+            dlg.ShowDialog();
+        }
+    }
+}
