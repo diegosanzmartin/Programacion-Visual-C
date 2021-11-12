5bf5410ee9efc53397e22dfdd2befd8b86109db8
diego-acc <diego.sm.acc@gmail.com>
2021-11-12 15:15:36 +0100

    v9.3.1

--- P5/Project/VentanaHija.Designer.cs
+++ P5/Project/VentanaHija.Designer.cs
@@ -30,7 +30,11 @@ namespace Project
         private void InitializeComponent()
         {
             this.m_PictureBox = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.imgenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ajustarVentanaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.m_PictureBox)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // m_PictureBox
@@ -38,12 +42,36 @@ namespace Project
             this.m_PictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.m_PictureBox.Location = new System.Drawing.Point(0, 0);
+            this.m_PictureBox.Location = new System.Drawing.Point(1, 0);
             this.m_PictureBox.Name = "m_PictureBox";
             this.m_PictureBox.Size = new System.Drawing.Size(565, 394);
             this.m_PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.m_PictureBox.TabIndex = 0;
             this.m_PictureBox.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.imgenToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(566, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // imgenToolStripMenuItem
+            // 
+            this.imgenToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ajustarVentanaToolStripMenuItem});
+            this.imgenToolStripMenuItem.Name = "imgenToolStripMenuItem";
+            this.imgenToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.imgenToolStripMenuItem.Text = "Imagen";
+            // 
+            // ajustarVentanaToolStripMenuItem
+            // 
+            this.ajustarVentanaToolStripMenuItem.Name = "ajustarVentanaToolStripMenuItem";
+            this.ajustarVentanaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ajustarVentanaToolStripMenuItem.Text = "Ajustar Ventana";
             // 
             // VentanaHija
             // 
@@ -51,9 +79,13 @@ namespace Project
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(566, 396);
             this.Controls.Add(this.m_PictureBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "VentanaHija";
             this.Text = "Form2";
             ((System.ComponentModel.ISupportInitialize)(this.m_PictureBox)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -62,5 +94,8 @@ namespace Project
         #endregion
 
         private System.Windows.Forms.PictureBox m_PictureBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem imgenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ajustarVentanaToolStripMenuItem;
     }
 }
